b58ad50cefc1a8c365b60e95c070cbc15fa400bb
Yash Sharma <YashSharma11170@gmail.com>
2024-09-21 02:55:10 +0530

    Initial comit

---
+++ script.js
@@ -0,0 +1,6 @@
+const typed = document.querySelector('#element')
+
+var type = new Typed('#element', {
+    strings: ['Graphic Designer', 'Web Designer', 'Frontend Developer'],
+    typeSpeed: 50,
+});
